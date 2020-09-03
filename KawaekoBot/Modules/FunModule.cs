d3514a3c769a2e49648facfa1f506678c726fd19
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-09-03 10:47:30 -0400

    UwU services

--- KawaekoBot/Modules/FunModule.cs
+++ KawaekoBot/Modules/FunModule.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using System;
 using Serilog;
+using Discord.WebSocket;
+using KawaekoBot.Services;
 
 namespace KawaekoBot.Modules
 {
@@ -31,7 +33,7 @@ namespace KawaekoBot.Modules
         [Alias("fortune")]
         [Remarks("8ball [question]")]
         [RequireBotPermission(ChannelPermission.SendMessages)]
-        public async Task EightBall(string question = null)
+        public async Task EightBall([Remainder ]string question = null)
         {
             await Task.Factory.StartNew(() => { _ = InvokeEightBallApi(question); });
         }
@@ -40,29 +42,85 @@ namespace KawaekoBot.Modules
         [Summary("Will give you a random meme from reddit")]
         [Remarks("meme")]
         [RequireBotPermission(ChannelPermission.SendMessages)]
-        public async Task Meme()
+        public async Task Meme(string subreddit = "")
         {
-            await Task.Factory.StartNew(() => { _ = InvokeMemeApi(); });
+            await Task.Factory.StartNew(() => { _ = InvokeMemeApi(subreddit); });
         }
 
-        private async Task InvokeMemeApi()
+        [Command("uwu")]
+        [Summary("I've been tracking uwu's, I will give you a user's count or the server's count")]
+        [Remarks("uwu [User (Optional)]")]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        public async Task UwU(SocketGuildUser user = null)
         {
-            try { HttpResponseMessage response = await httpClient.GetAsync("https://meme-api.herokuapp.com/gimme");
-                if (!response.IsSuccessStatusCode) await ReplyAsync("The meme machine is down, quick, call 911!");
-                else
-                {
-                    MemeResponse meme = JsonConvert.DeserializeObject<MemeResponse>(await response.Content.ReadAsStringAsync());
-                    EmbedBuilder memeBuilder = new EmbedBuilder()
-                    {
-                        Title = meme.title,
-                        Description = $"/r/{meme.subreddit}",
-                        ImageUrl = meme.url
-                    };
-                    await ReplyAsync(embed: memeBuilder.Build());
-                }
+            await Task.Factory.StartNew(() => { _ = InvokeUwUServices(user); });
+        }
+
+        private async Task InvokeUwUServices(SocketGuildUser user)
+        {
+            int uwuCount;
+            if (user != null && user.IsBot) 
+            {
+                await ReplyAsync("Cannot find the uwu score for a bot");
+                return;
             }
-            catch (Exception e){ Log.Error(e.Message); }
-            
+            switch (Context.Guild) 
+            {
+                case null:
+                    await ReplyAsync("Please use this command in a server");
+                    break;
+                default:
+                    UwUCounterService uwuCounterService = new UwUCounterService();
+                    uwuCount = await uwuCounterService.CountUwU(user, Context);
+                    await HandleUwUCount(uwuCount, user);
+                    break;
+            }
+        }
+
+        private async Task HandleUwUCount(int uwuCount, SocketGuildUser user)
+        {
+            switch (uwuCount)
+            {
+                case -1:
+                    await ReplyAsync("An critical error has occured while gathering your server's uwu score");
+                    break;
+                default:
+                    await HandleUserOrGuild(uwuCount, user);
+                    break;
+            }
+        }
+
+        private async Task HandleUserOrGuild(int uwuCount, SocketGuildUser user)
+        {
+            switch (user)
+            {
+                case null:
+                    await ReplyAsync($"{Context.Guild.Name}'s uwu score is: {uwuCount}");
+                    break;
+                default:
+                    await ReplyAsync($"{user.Username}'s uwu score is: {uwuCount}");
+                    break;
+            }
+        }
+
+        private async Task InvokeMemeApi(string subreddit)
+        {
+            string uri;
+            if (string.IsNullOrEmpty(subreddit)) uri = $"https://meme-api.herokuapp.com/gimme";
+            else uri = $"https://meme-api.herokuapp.com/gimme/{HttpUtility.UrlEncode(subreddit)}";
+            HttpResponseMessage response = await httpClient.GetAsync(requestUri: uri);
+            if (!response.IsSuccessStatusCode) await ReplyAsync("The meme machine is down, quick, call 911!");
+            else
+            {
+                MemeResponse meme = JsonConvert.DeserializeObject<MemeResponse>(await response.Content.ReadAsStringAsync());
+                EmbedBuilder memeBuilder = new EmbedBuilder()
+                {
+                    Title = meme.title,
+                    Description = $"/r/{meme.subreddit}",
+                    ImageUrl = meme.url
+                };
+                await ReplyAsync(embed: memeBuilder.Build());
+            }
         }
 
         private async Task InvokeEightBallApi(string question)
---
+++ KawaekoBot/Repository/UwURepository.cs
@@ -0,0 +1,145 @@
+﻿using Discord.WebSocket;
+using KawaekoBot.Util;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+using Serilog;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using KawaekoBot.Entities;
+using Discord.Commands;
+
+namespace KawaekoBot.Repository
+{
+    public class UwURepository
+    {
+        private SocketTextChannel _channel;
+        private SocketMessage _message;
+        private string _kawaekoId;
+        private string _guildId;
+        private string _userId;
+
+        private IMongoCollection<UwURecord> _collection = MongoDbClient.beanDatabase.GetCollection<UwURecord>("UwUCounter");
+
+        public UwURepository(SocketCommandContext commandContext)
+        {
+            this._kawaekoId = $"{commandContext.Guild.Id}{commandContext.User.Id}";
+            this._guildId = commandContext.Guild.Id.ToString();
+            this._userId = commandContext.User.Id.ToString();
+        }
+
+        public UwURepository(SocketUser user, SocketCommandContext commandContext)
+        {
+            this._kawaekoId = $"{commandContext.Guild.Id}{user.Id}";
+            this._guildId = commandContext.Guild.Id.ToString();
+            this._userId = user.Id.ToString();
+        }
+
+        public UwURepository(SocketTextChannel channel, SocketMessage message)
+        {
+            this._channel = channel;
+            this._message = message;
+            this._kawaekoId = $"{_channel.Guild.Id}{_message.Author.Id}";
+            this._guildId = _channel.Guild.Id.ToString();
+            this._userId = _message.Author.Id.ToString();
+        }
+
+        public async Task<List<UwURecord>> GetUwURecord()
+        {
+            try
+            {
+                var filterOne = Builders<UwURecord>.Filter.Eq("kawaekoId", _kawaekoId);
+                var results = await _collection.FindAsync<UwURecord>(filterOne);
+                return await results.ToListAsync();
+            }
+            catch (Exception e)
+            {
+                Log.Error($"User {_message.Author} with KawaekoId {_kawaekoId} failed retrieving from database for reason: {e.Message}");
+                return null;
+            }
+        }
+
+        public async Task InsertNewUwURecord()
+        {
+            var uwuDocument = new UwURecord
+            {
+                kawaekoId = _kawaekoId,
+                guildId = _guildId,
+                userId = _userId,
+                uwuCount = 1
+            };
+            try
+            {
+                await _collection.InsertOneAsync(uwuDocument);
+                Log.Information($"User {_message.Author} with KawaekoId {_kawaekoId} has been inserted into the database");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"User {_message.Author} with KawaekoId {_kawaekoId} failed inserting into database for reason: {e.Message}");
+            }
+        }
+
+        public async Task<int> GetUserUwUCount()
+        {
+            try
+            {
+                var filterUser = Builders<UwURecord>.Filter.Eq("kawaekoId", _kawaekoId);
+                var results = await _collection.FindAsync<UwURecord>(filterUser);
+                var uwuRecord = await results.FirstOrDefaultAsync();
+                switch (uwuRecord)
+                {
+                    case null:
+                        return 0;
+                    default:
+                        Log.Verbose(uwuRecord.ToString());
+                        Log.Verbose(uwuRecord.uwuCount.ToString());
+                        return uwuRecord.uwuCount;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Error retrieving uwu count for user with KawaekoId {_kawaekoId} for reason: {e.Message}");
+                return -1;
+            }
+        }
+
+        public async Task<int> GetServerUwUCount()
+        {
+            try
+            {
+                int uwuCount = 0;
+                var filterGuild = Builders<UwURecord>.Filter.Eq("guildId", _guildId);
+                var results = await _collection.FindAsync<UwURecord>(filterGuild);
+                foreach (var uwuRecord in await results.ToListAsync())
+                {
+                    uwuCount += uwuRecord.uwuCount;
+                }
+                return uwuCount;
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Error retrieving uwu count for server for reason: {e.Message}");
+                return -1;
+            }
+        }
+
+        public async Task IncrementUwURecord(UwURecord uwuRecord)
+        {
+            var filterOne = Builders<UwURecord>.Filter.Eq("kawaekoId", _kawaekoId);
+            var incrementUwUCount = Builders<UwURecord>.Update.Set("uwuCount", uwuRecord.uwuCount += 1);
+            try
+            {
+                await _collection.UpdateOneAsync(filterOne, incrementUwUCount);
+                Log.Information($"User {_message.Author} with KawaekoId {_kawaekoId} has been updated in the database");
+            }
+            catch (Exception e)
+            {
+                Log.Error($"User {_message.Author} with KawaekoId {_kawaekoId} failed updating database for reason: {e.Message}");
+            }
+        }
+    }
+}
